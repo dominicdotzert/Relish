0da86284d0b6453140b185d1ec71c9c55f6ae852
dominicdotzert <dominicdotzert@gmail.com>
2019-02-03 21:43:53 -0500

    Add Android splash screen

--- Relish/Relish.Android/MainActivity.cs
+++ Relish/Relish.Android/MainActivity.cs
@@ -9,14 +9,14 @@ using Android.OS;
 
 namespace Relish.Droid
 {
-    [Activity(Label = "Relish", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Relish", Icon = "@mipmap/icon", Theme = "@style/splashscreen", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
-
+            base.SetTheme(Resource.Style.MainTheme);
             base.OnCreate(savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
