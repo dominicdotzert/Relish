261b6d95a420a1a3b2c306b48ebb2f642c9e13d5
dominicdotzert <dominicdotzert@gmail.com>
2019-03-26 00:34:49 -0400

    Clean up code

--- Relish/Relish/Converters/BoolToColourConverter.cs
+++ Relish/Relish/Converters/BoolToColourConverter.cs
@@ -1,19 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Text;
 using Xamarin.Forms;
 
 namespace Relish.Converters
 {
+    /// <summary>
+    /// Converter to return a colour value based on the passed in boolean value.
+    /// </summary>
     public class BoolToColourConverter : IValueConverter
     {
+        /// <inheritdoc />
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             var val = (bool)value;
-            return val ? Color.Green : Color.Red;
+            return val ? Color.FromHex("118D25") : Color.Red;
         }
 
+        /// <inheritdoc />
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
